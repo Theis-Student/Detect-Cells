c4fc6a43867171953f2fd92b16160da3169da91d
Theis-Student <s255907@dtu.dk>
2026-09-16 16:07:46 +0200

    Almost finished with the basic algo

--- code_and_samples/main2.c
+++ code_and_samples/main2.c
@@ -1,14 +1,29 @@
 #include "cbmp.h"
 #include <stdio.h>
+#include <time.h> 
+#include <unistd.h> 
+
 int x = BMP_WIDTH;
 int y = BMP_HEIGTH;
 
+int i = BMP_WIDTH;
+int j = BMP_HEIGTH;
+
+int a = BMP_WIDTH;
+int b = BMP_HEIGTH;
+
+int countCells = 0;
+
+//buffer1 
+//buffer2
+
+int finish = 1;
+int countBlack = 0;
 
 
 unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 unsigned char gray_px[BMP_WIDTH][BMP_HEIGTH];
 unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-
 
 
 void read_bitmap(char * input_file_path,
@@ -37,6 +52,7 @@ int convert2Dto3D(void){
             output_image[x][y][2] = g;  // Blue
         }
     }
+    return 0;
 }
 
 int threshHold(void){
@@ -55,37 +71,103 @@ int threshHold(void){
 }
 
 int erosion(){
-    for (x = 0; x < BMP_WIDTH; x++){
-        for(y = 0; y < BMP_HEIGTH; y++){
+    int neighbors = 0;
+    unsigned char out[BMP_WIDTH][BMP_HEIGTH];
 
-        }
-    }
-    {
-        int neighbours = 0;
-        
-    }int neighbours(int x, int y){
-       int neighbours = 0;
-       if (image[x][y][0] == 0){
-           return 0;
-       }
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if ( )
-                    if (gameState[y + i][x + j] == 1) {
-                        neighbours++;
-                    }
+    for (x = 0;x < BMP_WIDTH; x++){
+        for (y = 0; y < BMP_HEIGTH; y++){
+            if (x-1 >= 0){
+                if(gray_px[x-1][y] > 0){
+                    neighbors++;
+                }
             }
+            if (x+1 <= BMP_WIDTH){
+                if(gray_px[x+1][y] > 0){
+                    neighbors++;
+                }
+            }
+            if(y - 1 >= 0){
+                if(gray_px[x][y-1] > 0){
+                    neighbors++;
+                }
+            }
+            
+            if (y + 1 <= BMP_HEIGTH){
+                if (gray_px[x][y+1]){
+                    neighbors++;
+                }
+            }
+            if (neighbors == 4){
+                out[x][y] = gray_px[x][y];
+            } else {
+                out[x][y] = 0;
+            }
+            //printf("%d",neighbors);
+            neighbors = 0;
+            }
+            
         }
-        if (neighbours < 4){
-            return 0;
-        } else {
-            return 1;
+    for(x = 0; x < BMP_WIDTH; x++){
+        for(y = 0; y < BMP_HEIGTH;y++){
+            gray_px[x][y] = out[x][y];
         }
     }
 }
+int detectCoconut(int x, int y){
+        for(a = 1; a < 13; a++){
+            for(b = 1; b < 13; b++){
+                if(gray_px[x+a][y+b] > 0){
+                    for(int n = 1; n < 13; n++){
+                        for(int m = 1; m < 13; m++){
+                            gray_px[x+n][y+m] = 0;
+                        }
+                    }
+                    countCells++;
+                    printf("%d ",countCells);
+                    return 0;
+                }
+            }
+        }
+        return 0;
+}
 
 int detectCells(){
-
+    countBlack = 0;
+    for(x = 0; x < BMP_WIDTH-14; x++){
+        for(y = 0; y < BMP_HEIGTH-14; y++){
+            // Top
+            for(i = 0; i < 14; i++){
+                if(gray_px[x+i][y] == 0){
+                    countBlack++;
+                }
+            }
+            // Bottom
+            for(i = 0; i < 13; i++){
+                if(gray_px[x+i][y+13] == 0){
+                    countBlack++;
+                }
+            }
+            //Left
+            for(j = 1; j < 14; j++){
+                if(gray_px[x][y+j] == 0){
+                    countBlack++;
+                }
+            }
+            //Right
+            for(j = 1; j < 13; j++){
+                if(gray_px[x+13][y+j] == 0){
+                    countBlack++;
+                }
+            }
+            if(countBlack == 52){
+                detectCoconut(x,y);
+            }
+            
+            //printf("%d ",countBlack);
+            countBlack = 0;
+            
+        }
+    }
 }
 
 void outImage(){
@@ -99,10 +181,29 @@ void write_bitmap(unsigned char input_image_array[BMP_WIDTH]
 
 int main(void){
     read_bitmap("samples/easy/1EASY.bmp", image);
-    printf("%d\n", image[0][0][0]);
     grayScale();
     threshHold();
     convert2Dto3D();
     write_bitmap(output_image, "samples/easy/1EASY_gray.bmp");
+    printf("%d ",countCells);
+
+    while(finish){
+    countBlack = 0;
+    erosion();
+    detectCells();
+    sleep(1);
+    convert2Dto3D();
+    write_bitmap(output_image, "samples/easy/1EASY_gray2.bmp");
+    for(x = 0; x < BMP_WIDTH; x++){
+        for(y = 0; y < BMP_HEIGTH; y++){
+            if(gray_px[x][y] == 0){
+                countBlack++;
+            }
+            if(countBlack == BMP_WIDTH*BMP_HEIGTH){
+                finish = 0;
+            }
+        }
+    }
+    }
     return 0;
 }
